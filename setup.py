cf8dfeeb3c4d14e391d38b4f863c1f2928a94629
Jon San Miguel <sanmiguelje@yahoo.com>
2015-01-15 16:29:17 -0800

    Make pip installable

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+  name='gaeSDK',
+  version='0.1.0',
+  author='Jon San Miguel',
+  author_email='jon.sanmiguel@optimizely.com',
+  packages=['google'],
+  url='https://github.com/optimizely/gae-sdk',
+  license='Apache License',
+  description='GAE SDK Pip installable Mirror',
+  long_description=open('README').read(),
+  install_requires=[
+  ],
+  test_requires=[
+  ],
+)
